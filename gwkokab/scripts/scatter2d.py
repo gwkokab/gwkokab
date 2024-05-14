ef488d8b461c150481e71db45d647fcfe79b8812
Qazalbash <meesumqazalbash@gmail.com>
2024-05-15 03:47:59 +0500

    add command line utility for 2d scatter plots, #67

---
+++ gwkokab/scripts/scatter2d.py
@@ -0,0 +1,104 @@
+#  Copyright 2023 The GWKokab Authors
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+
+from __future__ import annotations
+
+import argparse
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def make_parser() -> argparse.ArgumentParser:
+    """Create the command line argument parser.
+
+    This function creates the command line argument parser and returns it.
+
+    :return: the command line argument parser
+    """
+    parser = argparse.ArgumentParser(
+        description="Scatter 2D plotter.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        epilog="This script plots a 2D scatter plot.",
+    )
+    parser.add_argument(
+        "-d",
+        "--data",
+        help="data file path. Only .dat files are supported.",
+        required=True,
+        type=argparse.FileType("r"),
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        help="output file path",
+        required=True,
+        type=argparse.FileType("w"),
+    )
+    parser.add_argument(
+        "-x",
+        "--x-value-column-index",
+        help="index of the x-axis values",
+        required=True,
+        type=int,
+    )
+    parser.add_argument(
+        "-y",
+        "--y-value-column-index",
+        help="index of the y-axis values",
+        required=True,
+        type=int,
+    )
+    parser.add_argument(
+        "-t",
+        "--title",
+        help="title of the plot",
+        type=str,
+    )
+    parser.add_argument(
+        "-xl",
+        "--xlabel",
+        help="label of the x-axis",
+        default="x",
+        type=str,
+    )
+    parser.add_argument(
+        "-yl",
+        "--ylabel",
+        help="label of the y-axis",
+        default="y",
+        type=str,
+    )
+
+    return parser
+
+
+def main() -> None:
+    """Main function of the script."""
+    parser = make_parser()
+    args = parser.parse_args()
+
+    data = np.loadtxt(args.data.name)
+    x = data[:, args.x_value_column_index]
+    y = data[:, args.y_value_column_index]
+
+    plt.scatter(x, y)
+    plt.xlabel(args.xlabel)
+    plt.ylabel(args.ylabel)
+    plt.title(args.title)
+    plt.tight_layout()
+    plt.savefig(args.output.name)
+
+    args.data.close()
