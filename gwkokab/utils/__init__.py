cc5b1038a8fade98b7c30162050b8bee2f1d4b5f
Qazalbash <meesumqazalbash@gmail.com>
2024-06-09 01:06:07 +0500

    All necessary transformations in interesic parameters/coordinates

--- gwkokab/utils/__init__.py
+++ gwkokab/utils/__init__.py
@@ -14,9 +14,34 @@
 
 from __future__ import annotations
 
-from .mass_relations import (
+from .transformations import (
+    cart_to_polar as cart_to_polar,
+    cart_to_spherical as cart_to_spherical,
+    chi_cos_tilt_to_chiz as chi_cos_tilt_to_chiz,
     chirp_mass as chirp_mass,
+    delta_m as delta_m,
+    delta_m_to_symmetric_mass_ratio as delta_m_to_symmetric_mass_ratio,
+    m1_m2_chi1_chi2_costilt1_costilt2_to_chieff as m1_m2_chi1_chi2_costilt1_costilt2_to_chieff,
+    m1_m2_chi1_chi2_costilt1_costilt2_to_chiminus as m1_m2_chi1_chi2_costilt1_costilt2_to_chiminus,
+    m1_m2_chi1z_chi2z_to_chieff as m1_m2_chi1z_chi2z_to_chieff,
+    m1_m2_chi1z_chi2z_to_chiminus as m1_m2_chi1z_chi2z_to_chiminus,
+    m1_m2_chieff_chiminus_to_chi1z_chi2z as m1_m2_chieff_chiminus_to_chi1z_chi2z,
+    m1_m2_ordering as m1_m2_ordering,
+    m1_m2_to_Mc_eta as m1_m2_to_Mc_eta,
+    m1_times_m2 as m1_times_m2,
+    m1q_to_m2 as m1q_to_m2,
+    m2q_to_m1 as m2q_to_m1,
+    m_det_z_to_m_source as m_det_z_to_m_source,
+    M_q_to_m1_m2 as M_q_to_m1_m2,
+    m_source_z_to_m_det as m_source_z_to_m_det,
     mass_ratio as mass_ratio,
+    Mc_delta_chieff_chiminus_to_chi1z_chi2z as Mc_delta_chieff_chiminus_to_chi1z_chi2z,
+    MC_delta_to_m1_m2 as MC_delta_to_m1_m2,
+    Mc_eta_to_m1_m2 as Mc_eta_to_m1_m2,
+    polar_to_cart as polar_to_cart,
     reduced_mass as reduced_mass,
+    spherical_to_cart as spherical_to_cart,
     symmetric_mass_ratio as symmetric_mass_ratio,
+    symmetric_mass_ratio_to_delta_m as symmetric_mass_ratio_to_delta_m,
+    total_mass as total_mass,
 )
