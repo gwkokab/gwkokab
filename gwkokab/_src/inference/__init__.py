a4ab0eb593ea0aa643e6aa520a371ef3608ad7eb
Qazalbash <meesumqazalbash@gmail.com>
2024-06-23 03:48:25 +0500

    chore: public api for flowMChandler class

--- gwkokab/_src/inference/__init__.py
+++ gwkokab/_src/inference/__init__.py
@@ -13,6 +13,7 @@
 #  limitations under the License.
 
 
+from .flowMChandler import flowMChandler as flowMChandler
 from .hbm import BayesianHierarchicalModel as BayesianHierarchicalModel
 from .utils import (
     ModelPack as ModelPack,
