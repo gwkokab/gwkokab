53559b284c8ead5f3c4425b129a262680d9cd222
Qazalbash <meesumqazalbash@gmail.com>
2024-06-24 19:01:51 +0500

    chore: cart to polar test

--- tests/test_transformations.py
+++ tests/test_transformations.py
@@ -12,10 +12,12 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+import numpy as np
 import pytest
 from jax import numpy as jnp
 
 from gwkokab.utils.transformations import (
+    cart_to_polar,
     chirp_mass,
     delta_m,
     delta_m_to_symmetric_mass_ratio,
@@ -25,6 +27,7 @@ from gwkokab.utils.transformations import (
     m_det_z_to_m_source,
     m_source_z_to_m_det,
     mass_ratio,
+    polar_to_cart,
     reduced_mass,
     symmetric_mass_ratio,
     symmetric_mass_ratio_to_delta_m,
@@ -32,42 +35,66 @@ from gwkokab.utils.transformations import (
 )
 
 
-# These masses are in such a way that we check both integers
-# and floats along with the working of the functions
-_primary_masses = [1, 2, 3.0]
-_secondary_masses = [4.0, 5, 6.0]
-_redshifts = [0.1, 0.2, 0.7, 1]
+_primary_masses = np.unique(np.random.uniform(0.5, 200, 7))
+_secondary_masses = np.array([np.random.uniform(0.5, m) for m in _primary_masses])
+_redshifts = np.random.uniform(0, 1, 5)
+_rho = np.random.uniform(0, 10, 5)
+_thetas = np.random.uniform(0, 2 * np.pi, 10)
+_phis = np.random.uniform(0, np.pi, 10)
+_x = np.random.uniform(-10, 10, 10)
+_y = np.random.uniform(-10, 10, 10)
+_z = np.random.uniform(-10, 10, 10)
 
 
 @pytest.mark.parametrize("m1", _primary_masses)
 @pytest.mark.parametrize("m2", _secondary_masses)
 def test_different_mass_representations(m1, m2):
     m1m2 = m1 * m2
-    assert jnp.equal(m1m2, m1_times_m2(m1=m1, m2=m2))
+    assert jnp.allclose(m1m2, m1_times_m2(m1=m1, m2=m2))
     M = m1 + m2
-    assert jnp.equal(M, total_mass(m1=m1, m2=m2))
+    assert jnp.allclose(M, total_mass(m1=m1, m2=m2))
     q = m2 / m1
-    assert jnp.equal(q, mass_ratio(m1=m1, m2=m2))
-    assert jnp.equal(m1, m2_q_to_m1(m2=m2, q=q))
-    assert jnp.equal(m2, m1_q_to_m2(m1=m1, q=q))
+    assert jnp.allclose(q, mass_ratio(m1=m1, m2=m2))
+    assert jnp.allclose(m1, m2_q_to_m1(m2=m2, q=q))
+    assert jnp.allclose(m2, m1_q_to_m2(m1=m1, q=q))
     Mc = m1m2**0.6 * M**-0.2
-    assert jnp.equal(Mc, chirp_mass(m1=m1, m2=m2))
+    assert jnp.allclose(Mc, chirp_mass(m1=m1, m2=m2))
     eta = m1m2 * M**-2.0
-    assert jnp.equal(eta, symmetric_mass_ratio(m1=m1, m2=m2))
+    assert jnp.allclose(eta, symmetric_mass_ratio(m1=m1, m2=m2))
     Mr = m1m2 / M
-    assert jnp.equal(Mr, reduced_mass(m1=m1, m2=m2))
+    assert jnp.allclose(Mr, reduced_mass(m1=m1, m2=m2))
     delta = (m1 - m2) / M
-    assert jnp.equal(delta, delta_m(m1=m1, m2=m2))
+    assert jnp.allclose(delta, delta_m(m1=m1, m2=m2))
     delta_to_eta = (1 - delta**2) / 4
-    assert jnp.equal(delta_to_eta, delta_m_to_symmetric_mass_ratio(delta_m=delta))
+    assert jnp.allclose(delta_to_eta, delta_m_to_symmetric_mass_ratio(delta_m=delta))
     eta_to_delta = jnp.sqrt(1 - 4 * eta)
-    assert jnp.equal(eta_to_delta, symmetric_mass_ratio_to_delta_m(eta=eta))
+    assert jnp.allclose(eta_to_delta, symmetric_mass_ratio_to_delta_m(eta=eta))
 
 
 @pytest.mark.parametrize("m", _primary_masses + _secondary_masses)
 @pytest.mark.parametrize("z", _redshifts)
 def test_mass_and_reshift(m, z):
     m_source = m / (1.0 + z)
-    assert jnp.equal(m_source, m_det_z_to_m_source(m_det=m, z=z))
+    assert jnp.allclose(m_source, m_det_z_to_m_source(m_det=m, z=z))
     m_det = m * (1.0 + z)
-    assert jnp.equal(m_det, m_source_z_to_m_det(m_source=m, z=z))
+    assert jnp.allclose(m_det, m_source_z_to_m_det(m_source=m, z=z))
+
+
+@pytest.mark.parametrize("x", _x)
+@pytest.mark.parametrize("y", _y)
+def test_cart_to_polar(x, y):
+    r, theta = cart_to_polar(x=x, y=y)
+    r_ = jnp.sqrt(x**2 + y**2)
+    theta_ = jnp.arctan2(y, x)
+    assert jnp.allclose(r, r_)
+    assert jnp.allclose(theta, theta_)
+
+
+@pytest.mark.parametrize("r", _rho)
+@pytest.mark.parametrize("theta", _thetas)
+def test_polar_to_cart(r, theta):
+    x, y = polar_to_cart(r=r, theta=theta)
+    x_ = r * jnp.cos(theta)
+    y_ = r * jnp.sin(theta)
+    assert jnp.allclose(x, x_)
+    assert jnp.allclose(y, y_)
