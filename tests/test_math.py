86c166a67c2c945d105ce05b6f717a81754ecc7f
Qazalbash <meesumqazalbash@gmail.com>
2024-08-10 12:09:35 +0500

    chore: math util unit tests modified

--- tests/test_math.py
+++ tests/test_math.py
@@ -6,19 +6,19 @@ from gwkokab.utils.math import (
 )
 
 
-@pytest.mark.parametrize("alpha", np.random.uniform(1, 200, 5))
-@pytest.mark.parametrize("beta", np.random.uniform(1, 200, 5))
+@pytest.mark.parametrize(
+    "alpha, beta", [(10, 20), (30, 40), (60, 50), (70, 80), (100, 90)]
+)
 def test_beta_dist1(alpha, beta):
     mean, variance = beta_dist_concentrations_to_mean_variance(alpha, beta)
     alpha_, beta_ = beta_dist_mean_variance_to_concentrations(mean, variance)
-    print(alpha, alpha_)
-    print(beta, beta_)
     assert np.allclose(alpha, alpha_)
     assert np.allclose(beta, beta_)
 
 
-@pytest.mark.parametrize("mean", np.random.uniform(0, 1, 5))
-@pytest.mark.parametrize("var", np.random.uniform(0, 0.25, 5))
+@pytest.mark.parametrize(
+    "mean, var", [(0.1, 0.02), (0.2, 0.05), (0.3, 0.07), (0.4, 0.1), (0.5, 0.12)]
+)
 def test_beta_dist2(mean, var):
     alpha, beta = beta_dist_mean_variance_to_concentrations(mean, var)
     mean_, var_ = beta_dist_concentrations_to_mean_variance(alpha, beta)
