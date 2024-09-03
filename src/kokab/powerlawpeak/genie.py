bbe129e3560a98aba220d0070bafa12bd27de090
Qazalbash <meesumqazalbash@gmail.com>
2024-09-03 14:07:44 +0500

    chore: update imports in scripts

--- src/kokab/powerlawpeak/genie.py
+++ src/kokab/powerlawpeak/genie.py
@@ -12,6 +12,9 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+
+from __future__ import annotations
+
 from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
 
 from gwkokab.errors import banana_error_m1_m2
