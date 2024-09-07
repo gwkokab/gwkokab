9ffa4511347f7a3415a97122ac56cff185121e84
Qazalbash <meesumqazalbash@gmail.com>
2024-09-08 02:25:59 +0500

    refactor: add new genie script for n_pls_m_gs
    Added a new genie script for n_pls_m_gs in the kokab_genie_n_pls_m_gs module. This script calculates the population of binary systems with a power law mass distribution and multiple Gaussian spin distributions. It includes error handling and model options.
    
    Refactored the `utils` module to import the `regex` module along with the `genie_parser`

--- src/kokab/utils/__init__.py
+++ src/kokab/utils/__init__.py
@@ -12,4 +12,4 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from . import genie_parser as genie_parser
+from . import genie_parser as genie_parser, regex as regex
