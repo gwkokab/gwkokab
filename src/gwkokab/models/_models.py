2d47f13c565dff2d60e3833bc5e1b4e90b0d77f3
Qazalbash <meesumqazalbash@gmail.com>
2024-09-24 02:11:53 +0500

    chore: constraint event dimension changed in powerlaw primary mass ratio

--- src/gwkokab/models/_models.py
+++ src/gwkokab/models/_models.py
@@ -791,7 +791,7 @@ class PowerLawPrimaryMassRatio(Distribution):
             batch_shape=batch_shape, event_shape=(2,), validate_args=validate_args
         )
 
-    @constraints.dependent_property(is_discrete=False, event_dim=0)
+    @constraints.dependent_property(is_discrete=False, event_dim=1)
     def support(self) -> constraints.Constraint:
         return self._support
 
