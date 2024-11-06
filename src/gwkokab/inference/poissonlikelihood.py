fix: reorder the mean and exponential of VT calculation in PoissonLikelihood and PopulationFactory

src/gwkokab/inference/poissonlikelihood.py
@@ -207,7 +207,7 @@ class PoissonLikelihood:
         :return: Integral.
         """
         values = model.component_sample(key, (N,))[..., self.vt_params_index]
-        VT_fn = lambda xx: jnp.exp(jnp.mean(self.logVT(xx)))
+        VT_fn = lambda xx: jnp.mean(jnp.exp(self.logVT(xx)))
         VT = jax.vmap(VT_fn, in_axes=1)(values)
         rates = jnp.exp(model._log_scales)
         return self.time * jnp.dot(VT, rates)

src/gwkokab/population/_factory.py
@@ -101,7 +101,7 @@ class PopulationFactory:
         r"""Calculates the expected rate."""
         N = int(5e4)
         values = self.model.component_sample(key, (N,))[..., self.vt_selection_mask]
-        VT_fn = lambda xx: jnp.exp(jnp.mean(self.logVT_fn(xx)))
+        VT_fn = lambda xx: jnp.mean(jnp.exp(self.logVT_fn(xx)))
         VT = jax.vmap(VT_fn, in_axes=1)(values)
         rates = jnp.exp(self.model._log_scales)
         return self.analysis_time * jnp.dot(VT, rates)