74d003108f0ea962b5d5dae0b1c0584cb9dccf10
Meesum Qazalbash <meesumqazalbash@gmail.com>
2024-12-16 05:30:14 +0500

    fix: improve duplicate matching logic in regex utility (#399)
    * fix: improve duplicate matching logic in regex utility
    
    * fix: enhance regex matching logic to handle list values and improve test coverage

--- tests/kokab/utils/test_regex.py
+++ tests/kokab/utils/test_regex.py
@@ -17,13 +17,18 @@ from kokab.utils.regex import match_all
 
 
 def test_match_all():
-    strings = ["alpha_0", "beta_0", "beta_1", "gamma_10"]
+    strings = ["alpha_0", "beta_0", "beta_1", "gamma_10", "alpha_1", "delta_1"]
     pattern_dict_with_val = {
         "beta_[0-9]+": 0.1,
         "gamma_[0-9]+": 0.2,
+        "delta_[0-9]+": [0.3, 0.4],
         "alpha_0": "beta_0",
+        "alpha_1": "delta_1",
     }
     matches = match_all(strings, pattern_dict_with_val)
+    print(matches)
     assert matches["alpha_0"] == matches["beta_0"]
     assert matches["beta_1"] == 0.1
     assert matches["gamma_10"] == 0.2
+    assert matches["alpha_1"] == "delta_1"
+    assert matches["delta_1"] == [0.3, 0.4]
