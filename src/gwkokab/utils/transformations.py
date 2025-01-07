ef55c1ce5d895e272f4e7a21e12cbcf276910b97
Qazalbash <meesumqazalbash@gmail.com>
2025-01-07 15:46:17 +0500

    chore: using jit decorator for transformations

--- src/gwkokab/utils/transformations.py
+++ src/gwkokab/utils/transformations.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 
+from functools import partial
 from typing import Optional
 
 import jax
@@ -36,11 +37,13 @@ def total_mass(m1: ArrayLike, m2: ArrayLike) -> ArrayLike:
     return m1 + m2
 
 
+@partial(jax.jit, inline=True)
 def _mass_ratio_from_m1_m2(m1: ArrayLike, m2: ArrayLike) -> ArrayLike:
     safe_m1 = jnp.where(m1 == 0.0, 1.0, m1)
     return jnp.where(m1 == 0.0, jnp.inf, m2 / safe_m1)
 
 
+@partial(jax.jit, inline=True)
 def _mass_ratio_from_eta(eta: ArrayLike) -> ArrayLike:
     """
     implementation reference: https://lscsoft.docs.ligo.org/bilby/api/bilby.gw.conversion.symmetric_mass_ratio_to_mass_ratio.html
@@ -65,10 +68,11 @@ def mass_ratio(
     if m1 is not None and m2 is not None and eta is not None:
         raise ValueError("Only one of (m1, m2) or eta should be provided.")
     if eta is not None:
-        return jax.jit(_mass_ratio_from_eta, inline=True)(eta=eta)
-    return jax.jit(_mass_ratio_from_m1_m2, inline=True)(m1=m1, m2=m2)
+        return _mass_ratio_from_eta(eta=eta)
+    return _mass_ratio_from_m1_m2(m1=m1, m2=m2)
 
 
+@partial(jax.jit, inline=True)
 def _chirp_mass(m1: ArrayLike, m2: ArrayLike) -> ArrayLike:
     M = total_mass(m1=m1, m2=m2)
     m1m2 = m1_times_m2(m1=m1, m2=m2)
@@ -80,9 +84,10 @@ def chirp_mass(m1: ArrayLike, m2: ArrayLike) -> ArrayLike:
     .. math::
         M_c(m_1, m_2) = \frac{(m_1m_2)^{3/5}}{(m_1 + m_2)^{1/5}}
     """
-    return jax.jit(_chirp_mass, inline=True)(m1=m1, m2=m2)
+    return _chirp_mass(m1=m1, m2=m2)
 
 
+@partial(jax.jit, inline=True)
 def _log_chirp_mass(m1: ArrayLike, m2: ArrayLike) -> ArrayLike:
     M = total_mass(m1=m1, m2=m2)
     return 0.6 * (jnp.log(m1) + jnp.log(m2)) - 0.2 * jnp.log(M)
@@ -93,18 +98,20 @@ def log_chirp_mass(m1: ArrayLike, m2: ArrayLike) -> ArrayLike:
     .. math::
         \log(M_c(m_1, m_2)) = 3/5\times (\log(m_1) + \log(m_2)) - \log(m_1 + m_2)/5
     """
-    return jax.jit(_log_chirp_mass, inline=True)(m1=m1, m2=m2)
+    return _log_chirp_mass(m1=m1, m2=m2)
 
 
+@partial(jax.jit, inline=True)
 def _symmetric_mass_ratio_from_m1_m2(m1: ArrayLike, m2: ArrayLike) -> ArrayLike:
     M = total_mass(m1=m1, m2=m2)
     m1m2 = m1_times_m2(m1=m1, m2=m2)
     return m1m2 * jnp.power(M, -2.0)
 
 
+@partial(jax.jit, inline=True)
 def _symmetric_mass_ratio_from_q(q: ArrayLike) -> ArrayLike:
     safe_q = jnp.where(q == -1.0, 1.0, q)
-    return jnp.where(q == -1.0, jnp.inf, safe_q / jnp.square(1 + safe_q))
+    return jnp.where(q == -1.0, jnp.inf, safe_q / jnp.square(1.0 + safe_q))
 
 
 def symmetric_mass_ratio(
@@ -120,10 +127,11 @@ def symmetric_mass_ratio(
     if m1 is not None and m2 is not None and q is not None:
         raise ValueError("Only one of (m1, m2) or q should be provided.")
     if q is not None:
-        return jax.jit(_symmetric_mass_ratio_from_q, inline=True)(q=q)
-    return jax.jit(_symmetric_mass_ratio_from_m1_m2, inline=True)(m1=m1, m2=m2)
+        return _symmetric_mass_ratio_from_q(q=q)
+    return _symmetric_mass_ratio_from_m1_m2(m1=m1, m2=m2)
 
 
+@partial(jax.jit, inline=True)
 def _reduced_mass(m1: ArrayLike, m2: ArrayLike) -> ArrayLike:
     M = total_mass(m1=m1, m2=m2)
     m1m2 = m1_times_m2(m1=m1, m2=m2)
@@ -135,9 +143,10 @@ def reduced_mass(m1: ArrayLike, m2: ArrayLike) -> ArrayLike:
     .. math::
         M_r(m_1, m_2) = \frac{m_1m_2}{m_1 + m_2}
     """
-    return jax.jit(_reduced_mass, inline=True)(m1=m1, m2=m2)
+    return _reduced_mass(m1=m1, m2=m2)
 
 
+@partial(jax.jit, inline=True)
 def _delta_m(m1: ArrayLike, m2: ArrayLike) -> ArrayLike:
     diff = m1 - m2
     M = total_mass(m1=m1, m2=m2)
@@ -149,13 +158,14 @@ def delta_m(m1: ArrayLike, m2: ArrayLike) -> ArrayLike:
     .. math::
         \delta_m(m_1, m_2) = \frac{m_1 - m_2}{m_1 + m_2}
     """
-    return jax.jit(_delta_m, inline=True)(m1=m1, m2=m2)
+    return _delta_m(m1=m1, m2=m2)
 
 
+@partial(jax.jit, inline=True)
 def _delta_m_to_symmetric_mass_ratio(delta_m: ArrayLike) -> ArrayLike:
-    delta_m_sq = jnp.square(delta_m)  # delta_m^2
-    eta = 0.25 * (1 - delta_m_sq)  # (1 - delta_m^2) / 4
-    return eta
+    δ_m_sq = jnp.square(delta_m)  # delta_m^2
+    η = 0.25 * (1.0 - δ_m_sq)  # (1 - delta_m^2) / 4
+    return η
 
 
 def delta_m_to_symmetric_mass_ratio(delta_m: ArrayLike) -> ArrayLike:
@@ -163,13 +173,14 @@ def delta_m_to_symmetric_mass_ratio(delta_m: ArrayLike) -> ArrayLike:
     .. math::
         \eta(\delta_m) = \frac{1 - \delta_m^2}{4}
     """
-    return jax.jit(_delta_m_to_symmetric_mass_ratio, inline=True)(delta_m=delta_m)
+    return _delta_m_to_symmetric_mass_ratio(delta_m=delta_m)
 
 
+@partial(jax.jit, inline=True)
 def _symmetric_mass_ratio_to_delta_m(eta: ArrayLike) -> ArrayLike:
-    eta_4 = jnp.multiply(eta, 4)  #  eta*4
-    delta_m = jnp.sqrt(jnp.subtract(1, eta_4))  # sqrt(1 - 4 * eta)
-    return delta_m
+    η_4 = jnp.multiply(eta, 4)  #  eta*4
+    δ_m = jnp.sqrt(jnp.subtract(1.0, η_4))  # sqrt(1 - 4 * eta)
+    return δ_m
 
 
 def symmetric_mass_ratio_to_delta_m(eta: ArrayLike) -> ArrayLike:
@@ -177,9 +188,10 @@ def symmetric_mass_ratio_to_delta_m(eta: ArrayLike) -> ArrayLike:
     .. math::
         \delta_m(\eta) = \sqrt{1 - 4\eta}
     """
-    return jax.jit(_symmetric_mass_ratio_to_delta_m, inline=True)(eta=eta)
+    return _symmetric_mass_ratio_to_delta_m(eta=eta)
 
 
+@partial(jax.jit, inline=True)
 def _m_det_z_to_m_source(m_det: ArrayLike, z: ArrayLike) -> ArrayLike:
     safe_one_more_z = jnp.where(z == -1.0, 1.0, jnp.add(1.0, z))
     m_source = jnp.where(z == -1.0, jnp.inf, m_det / safe_one_more_z)
@@ -191,9 +203,10 @@ def m_det_z_to_m_source(m_det: ArrayLike, z: ArrayLike) -> ArrayLike:
     .. math::
         m_{\text{source}}(m_{\text{det}}, z) = \frac{m_{\text{det}}}{1 + z}
     """
-    return jax.jit(_m_det_z_to_m_source, inline=True)(m_det=m_det, z=z)
+    return _m_det_z_to_m_source(m_det=m_det, z=z)
 
 
+@partial(jax.jit, inline=True)
 def _m_source_z_to_m_det(m_source: ArrayLike, z: ArrayLike) -> ArrayLike:
     return m_source * (1.0 + z)
 
@@ -203,7 +216,7 @@ def m_source_z_to_m_det(m_source: ArrayLike, z: ArrayLike) -> ArrayLike:
     .. math::
         m_{\text{det}}(m_{\text{source}}, z) = m_{\text{source}}(1 + z)
     """
-    return jax.jit(_m_source_z_to_m_det, inline=True)(m_source=m_source, z=z)
+    return _m_source_z_to_m_det(m_source=m_source, z=z)
 
 
 def m1_q_to_m2(m1: ArrayLike, q: ArrayLike) -> ArrayLike:
@@ -214,6 +227,7 @@ def m1_q_to_m2(m1: ArrayLike, q: ArrayLike) -> ArrayLike:
     return m1 * q
 
 
+@partial(jax.jit, inline=True)
 def _m2_q_to_m1(m2: ArrayLike, q: ArrayLike) -> ArrayLike:
     safe_q = jnp.where(q == 0.0, 1.0, q)
     m1 = jnp.where(q == 0.0, jnp.inf, m2 / safe_q)
@@ -225,7 +239,7 @@ def m2_q_to_m1(m2: ArrayLike, q: ArrayLike) -> ArrayLike:
     .. math::
         m_1(m_2, q) = \frac{m_2}{q}
     """
-    return jax.jit(_m2_q_to_m1, inline=True)(m2=m2, q=q)
+    return _m2_q_to_m1(m2=m2, q=q)
 
 
 def chi_costilt_to_chiz(chi: ArrayLike, costilt: ArrayLike) -> ArrayLike:
@@ -236,14 +250,15 @@ def chi_costilt_to_chiz(chi: ArrayLike, costilt: ArrayLike) -> ArrayLike:
     return chi * costilt
 
 
+@partial(jax.jit, inline=True)
 def _m1_m2_chi1z_chi2z_to_chiminus(
     m1: ArrayLike, m2: ArrayLike, chi1z: ArrayLike, chi2z: ArrayLike
 ) -> ArrayLike:
-    m1_chi1z = m1 * chi1z
-    m2_chi2z = m2 * chi2z
+    m1_χ1z = m1 * chi1z
+    m2_χ2z = m2 * chi2z
     M = total_mass(m1=m1, m2=m2)
-    diff = m1_chi1z - m2_chi2z
-    return jnp.where(M == 0, jnp.inf, diff / M)
+    diff = m1_χ1z - m2_χ2z
+    return jnp.where(M == 0.0, jnp.inf, diff / M)
 
 
 def m1_m2_chi1z_chi2z_to_chiminus(
@@ -253,25 +268,25 @@ def m1_m2_chi1z_chi2z_to_chiminus(
     .. math::
         \chi_{\text{minus}}(m_1, m_2, \chi_{1z}, \chi_{2z}) = \frac{m_1\chi_{1z} - m_2\chi_{2z}}{m_1 + m_2}
     """
-    return jax.jit(_m1_m2_chi1z_chi2z_to_chiminus, inline=True)(
-        m1=m1, m2=m2, chi1z=chi1z, chi2z=chi2z
-    )
+    return _m1_m2_chi1z_chi2z_to_chiminus(m1=m1, m2=m2, chi1z=chi1z, chi2z=chi2z)
 
 
+@partial(jax.jit, inline=True)
 def _chi_eff_by_m1_m2(
     m1: ArrayLike, m2: ArrayLike, chi1z: ArrayLike, chi2z: ArrayLike
 ) -> ArrayLike:
-    m1_chi1z = m1 * chi1z
-    m2_chi2z = m2 * chi2z
+    m1_χ1z = m1 * chi1z
+    m2_χ2z = m2 * chi2z
     M = total_mass(m1=m1, m2=m2)
-    m_dot_chi = m1_chi1z + m2_chi2z
-    return jnp.where(M == 0, jnp.inf, m_dot_chi / M)
+    m_dot_χ = m1_χ1z + m2_χ2z
+    return jnp.where(M == 0, jnp.inf, m_dot_χ / M)
 
 
+@partial(jax.jit, inline=True)
 def _chi_eff_by_q(q: ArrayLike, chi1z: ArrayLike, chi2z: ArrayLike) -> ArrayLike:
     safe_q = jnp.where(q == -1.0, 1.0, q)
-    safe_chi_eff = jnp.where(q == -1.0, 1.0, (chi1z + safe_q * chi2z) / (1 + safe_q))
-    return safe_chi_eff
+    safe_χ_eff = jnp.where(q == -1.0, 1.0, (chi1z + safe_q * chi2z) / (1 + safe_q))
+    return safe_χ_eff
 
 
 def chi_eff(
@@ -292,12 +307,11 @@ def chi_eff(
     if m1 is not None and m2 is not None and q is not None:
         raise ValueError("Only one of (m1, m2) or q should be provided.")
     if q is not None:
-        return jax.jit(_chi_eff_by_q, inline=True)(q=q, chi1z=chi1z, chi2z=chi2z)
-    return jax.jit(_chi_eff_by_m1_m2, inline=True)(
-        m1=m1, m2=m2, chi1z=chi1z, chi2z=chi2z
-    )
+        return _chi_eff_by_q(q=q, chi1z=chi1z, chi2z=chi2z)
+    return _chi_eff_by_m1_m2(m1=m1, m2=m2, chi1z=chi1z, chi2z=chi2z)
 
 
+@partial(jax.jit, inline=True)
 def _chi_a(chi1z: ArrayLike, chi2z: ArrayLike) -> ArrayLike:
     return (chi1z + chi2z) * 0.5
 
@@ -307,7 +321,7 @@ def chi_a(chi1z: ArrayLike, chi2z: ArrayLike) -> ArrayLike:
     .. math::
         \chi_a(\chi_{1z}, \chi_{2z}) = \frac{\chi_{1z} + \chi_{2z}}{2}
     """
-    return jax.jit(_chi_a, inline=True)(chi1z=chi1z, chi2z=chi2z)
+    return _chi_a(chi1z=chi1z, chi2z=chi2z)
 
 
 def m1_m2_chi1_chi2_costilt1_costilt2_to_chieff(
@@ -324,9 +338,9 @@ def m1_m2_chi1_chi2_costilt1_costilt2_to_chieff(
         \chi_{\text{eff}}(m_1, m_2, \chi_1, \chi_2, \cos(\theta_1), \cos(\theta_2)) =
         \frac{m_1\chi_1\cos(\theta_1) + m_2\chi_2\cos(\theta_2)}{m_1 + m_2}
     """
-    chi1z = chi_costilt_to_chiz(chi=chi1, costilt=costilt1)
-    chi2z = chi_costilt_to_chiz(chi=chi2, costilt=costilt2)
-    return chi_eff(m1=m1, m2=m2, chi1z=chi1z, chi2z=chi2z)
+    χ1z = chi_costilt_to_chiz(chi=chi1, costilt=costilt1)
+    χ2z = chi_costilt_to_chiz(chi=chi2, costilt=costilt2)
+    return chi_eff(m1=m1, m2=m2, chi1z=χ1z, chi2z=χ2z)
 
 
 def m1_m2_chi1_chi2_costilt1_costilt2_to_chiminus(
@@ -343,9 +357,9 @@ def m1_m2_chi1_chi2_costilt1_costilt2_to_chiminus(
         \chi_{\text{minus}}(m_1, m_2, \chi_1, \chi_2, \cos(\theta_1), \cos(\theta_2)) =
         \frac{m_1\chi_1\cos(\theta_1) - m_2\chi_2\cos(\theta_2)}{m_1 + m_2}
     """
-    chi1z = chi_costilt_to_chiz(chi=chi1, costilt=costilt1)
-    chi2z = chi_costilt_to_chiz(chi=chi2, costilt=costilt2)
-    return m1_m2_chi1z_chi2z_to_chiminus(m1=m1, m2=m2, chi1z=chi1z, chi2z=chi2z)
+    χ1z = chi_costilt_to_chiz(chi=chi1, costilt=costilt1)
+    χ2z = chi_costilt_to_chiz(chi=chi2, costilt=costilt2)
+    return m1_m2_chi1z_chi2z_to_chiminus(m1=m1, m2=m2, chi1z=χ1z, chi2z=χ2z)
 
 
 def m1_m2_chieff_chiminus_to_chi1z_chi2z(
@@ -361,13 +375,13 @@ def m1_m2_chieff_chiminus_to_chi1z_chi2z(
         \end{align*}
     """
     half_M = jnp.multiply(0.5, total_mass(m1=m1, m2=m2))  # M/2
-    chi1z = jnp.divide(
+    χ1z = jnp.divide(
         jnp.multiply(half_M, jnp.add(chieff, chiminus)), m1
     )  # chi1z = M/2 * (chieff + chiminus) / m1
-    chi2z = jnp.divide(
+    χ2z = jnp.divide(
         jnp.multiply(half_M, jnp.subtract(chieff, chiminus)), m2
     )  # chi2z = M/2 * (chieff - chiminus) / m2
-    return chi1z, chi2z
+    return χ1z, χ2z
 
 
 def Mc_eta_to_m1_m2(Mc: ArrayLike, eta: ArrayLike) -> tuple[ArrayLike, ArrayLike]:
@@ -378,21 +392,19 @@ def Mc_eta_to_m1_m2(Mc: ArrayLike, eta: ArrayLike) -> tuple[ArrayLike, ArrayLike
             m_2(M_c, \eta) &= \frac{M_c}{2} \eta^{-0.6} (1 - \sqrt{1 - 4\eta})
         \end{align*}
     """
-    delta_sq = jnp.subtract(1, jnp.multiply(4.0, eta))  # 1 - 4 * eta
-    delta_sq = jnp.maximum(
-        delta_sq, jnp.zeros_like(delta_sq)
-    )  # to avoid negative values
-    delta = jnp.sqrt(delta_sq)  # sqrt(1 - 4 * eta)
+    δ_sq = jnp.subtract(1, jnp.multiply(4.0, eta))  # 1 - 4 * eta
+    δ_sq = jnp.maximum(δ_sq, jnp.zeros_like(δ_sq))  # to avoid negative values
+    δ = jnp.sqrt(δ_sq)  # sqrt(1 - 4 * eta)
     half_Mc = jnp.multiply(0.5, Mc)  # Mc/2
-    eta_pow_neg_point_six = jnp.power(eta, -0.6)  # eta^-0.6
-    half_Mc_times_eta_pow_neg_point_six = jnp.multiply(
-        half_Mc, eta_pow_neg_point_six
+    η_pow_neg_point_six = jnp.power(eta, -0.6)  # eta^-0.6
+    half_Mc_times_η_pow_neg_point_six = jnp.multiply(
+        half_Mc, η_pow_neg_point_six
     )  # Mc/2 * eta^-0.6
     m2 = jnp.multiply(
-        half_Mc_times_eta_pow_neg_point_six, jnp.subtract(1.0, delta)
+        half_Mc_times_η_pow_neg_point_six, jnp.subtract(1.0, δ)
     )  # m2 = Mc/2 * eta^-0.6 * (1 - delta)
     m1 = jnp.multiply(
-        half_Mc_times_eta_pow_neg_point_six, jnp.add(1.0, delta)
+        half_Mc_times_η_pow_neg_point_six, jnp.add(1.0, δ)
     )  # m1 = Mc/2 * eta^-0.6 * (1 + delta)
     return m1, m2
 
@@ -419,8 +431,8 @@ def cart_to_polar(x: ArrayLike, y: ArrayLike) -> tuple[ArrayLike, ArrayLike]:
         \end{align*}
     """
     r = jnp.sqrt(x * x + y * y)
-    theta = jnp.arctan2(y, x)
-    return r, theta
+    θ = jnp.arctan2(y, x)
+    return r, θ
 
 
 def spherical_to_cart(
@@ -446,15 +458,13 @@ def cart_to_spherical(
     r"""
     .. math::
         \begin{align*}
-            r(x, y, z) &= \sqrt{x^2 + y^2 + z^2} \\
-            \theta(x, y, z) &= \arccos\left(\frac{z}{r}\right) \\
+            \rho(x, y, z) &= \sqrt{x^2 + y^2 + z^2} \\
+            \theta(x, y, z) &= \arccos\left(\frac{z}{\rho}\right) \\
             \phi(x, y, z) &= \arctan\left(\frac{y}{x}\right)
         \end{align*}
     """
-    r = jnp.sqrt(x * x + y * y + z * z)  # r = sqrt(x^2 + y^2 + z^2)
-    safe_r = jnp.where(r == 0.0, 1.0, r)
-    theta = jnp.arccos(
-        jnp.where(r == 0.0, jnp.inf, z / safe_r)
-    )  # theta = arccos(z / r)
-    phi = jnp.arctan2(y, x)  # phi = arctan(y / x)
-    return r, theta, phi
+    ρ = jnp.sqrt(x * x + y * y + z * z)
+    safe_ρ = jnp.where(ρ == 0.0, 1.0, ρ)
+    θ = jnp.arccos(jnp.where(ρ == 0.0, jnp.inf, z / safe_ρ))
+    φ = jnp.arctan2(y, x)
+    return ρ, θ, φ
