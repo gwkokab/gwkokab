5131df32aa32893a17b5e0d70f2fe210c8e482f1
Qazalbash <meesumqazalbash@gmail.com>
2025-01-14 20:09:52 +0500

    fix: update test_match_all to use set for delta_[0-9]+ values

--- tests/kokab/utils/test_regex.py
+++ tests/kokab/utils/test_regex.py
@@ -21,14 +21,13 @@ def test_match_all():
     pattern_dict_with_val = {
         "beta_[0-9]+": 0.1,
         "gamma_[0-9]+": 0.2,
-        "delta_[0-9]+": [0.3, 0.4],
+        "delta_[0-9]+": {0.3, 0.4},
         "alpha_0": "beta_0",
         "alpha_1": "delta_1",
     }
     matches = match_all(strings, pattern_dict_with_val)
-    print(matches)
     assert matches["alpha_0"] == matches["beta_0"]
     assert matches["beta_1"] == 0.1
     assert matches["gamma_10"] == 0.2
     assert matches["alpha_1"] == "delta_1"
-    assert matches["delta_1"] == [0.3, 0.4]
+    assert matches["delta_1"] == pattern_dict_with_val["delta_[0-9]+"]
