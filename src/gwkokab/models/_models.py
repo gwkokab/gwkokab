fix: simplify log probability calculation in SmoothedPowerlawAndPeak class

src/gwkokab/models/_models.py
@@ -1044,11 +1044,7 @@ class SmoothedPowerlawAndPeak(Distribution):
             (m2 - self.mmin) / jnp.where(self.delta == 0.0, 1.0, self.delta)
         )
         log_prob_q = jnp.where(
-            jnp.less_equal(m1, self.mmin),
-            -jnp.inf,
-            doubly_truncated_power_law_log_prob(
-                x=q, alpha=self.beta, low=self.mmin / m1, high=1.0
-            ),
+            jnp.less_equal(m1, self.mmin), -jnp.inf, self.beta * jnp.log(q)
         )
         return log_prob_q + log_smoothing_q
 