fix: apply stop_gradient to Z_powerlaw and Z_gaussian in SmoothedPowerlawAndPeak and ImportanceSamplingPoissonMean

src/gwkokab/models/_models.py
@@ -1088,13 +1088,13 @@ class SmoothedPowerlawAndPeak(Distribution):
         m1 = value[..., 0]
 
         log_prob_m1 = self._log_prob_m1(
-            m1, Z_powerlaw=self._Z_powerlaw, Z_gaussian=self._Z_gaussian
+            m1,
+            Z_powerlaw=lax.stop_gradient(self._Z_powerlaw),
+            Z_gaussian=lax.stop_gradient(self._Z_gaussian),
         )
 
         log_prob_q = self._log_prob_q(value)
 
-        log_Z_q = jnp.log(self._Z_q(m1, log_prob_q.shape))
+        log_Z_q = lax.stop_gradient(jnp.log(self._Z_q(m1, log_prob_q.shape)))
 
-        log_Z = lax.stop_gradient(log_Z_q)
-
-        return self.log_rate + log_prob_m1 + log_prob_q - log_Z
+        return self.log_rate + log_prob_m1 + log_prob_q - log_Z_q

src/kokab/one_powerlaw_one_peak/poisson_mean.py
@@ -165,10 +165,11 @@ class ImportanceSamplingPoissonMean(PoissonMeanABC):
             )
         )
 
-        total_estimated_rate = (
-            (1 - model.lambda_peak) / model._Z_powerlaw * rate_powerlaw
-            + model.lambda_peak / model._Z_gaussian * rate_gaussian
-        )
+        total_estimated_rate = (1 - model.lambda_peak) / lax.stop_gradient(
+            model._Z_powerlaw
+        ) * rate_powerlaw + model.lambda_peak / lax.stop_gradient(
+            model._Z_gaussian
+        ) * rate_gaussian
 
         total_estimated_rate = total_estimated_rate * jnp.exp(model.log_rate)
 