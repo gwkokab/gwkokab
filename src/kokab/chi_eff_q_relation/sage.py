fix: update log_ref_priors calculation in sage.py to use REDSHIFT prior

src/kokab/chi_eff_q_relation/sage.py
@@ -18,7 +18,6 @@ import warnings
 from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
 from glob import glob
 
-import numpy as np
 from jax import numpy as jnp, random as jrd
 from numpyro.distributions import Uniform
 
@@ -142,7 +141,7 @@ def main() -> None:
         raise ValueError("Invalid estimator for expected rate.")
 
     data = get_posterior_data(glob(POSTERIOR_REGEX), POSTERIOR_COLUMNS)
-    log_ref_priors = [np.zeros(d.shape) for d in data]
+    log_ref_priors = [REDSHIFT.prior.log_prob(d[..., 4]) for d in data]
 
     poisson_likelihood = PoissonLikelihood(
         model=model,