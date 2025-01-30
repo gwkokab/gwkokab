fix: apply `lax.stop_gradient` to normalization in total estimated rate calculation in `ImportanceSamplingPoissonMean` class

src/kokab/one_powerlaw_one_peak/poisson_mean.py
@@ -171,8 +171,10 @@ class ImportanceSamplingPoissonMean(PoissonMeanABC):
         )
 
         total_estimated_rate = (
-            (1 - model.lambda_peak) * rate_powerlaw / model._Z_powerlaw
-        ) + (model.lambda_peak * rate_gaussian / model._Z_gaussian)
+            (1 - model.lambda_peak)
+            * rate_powerlaw
+            / lax.stop_gradient(model._Z_powerlaw)
+        ) + (model.lambda_peak * rate_gaussian / lax.stop_gradient(model._Z_gaussian))
 
         total_estimated_rate = total_estimated_rate * jnp.exp(model.log_rate)
 