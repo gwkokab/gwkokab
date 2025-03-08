60424c40320300f91a634a45a71442681fd83bdf
Qazalbash <meesumqazalbash@gmail.com>
2025-03-09 01:43:12 +0500

    chore: remove `gwpopulation` dependency from VTS module

--- src/gwkokab/vts/_pdet/__init__.py
+++ src/gwkokab/vts/_pdet/__init__.py
@@ -1,9 +1,1 @@
-import gwpopulation
-
-
-# We only support JAX backend
-# https://github.com/ColmTalbot/wcosmo/issues/3#issuecomment-2426836308
-gwpopulation.set_backend("jax")
-
-
 from .O3 import pdet_O3 as pdet_O3
