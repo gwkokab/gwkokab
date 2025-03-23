3fd14d1f77c600c0bad5efa5846bbff45d81f108
Meesum Qazalbash <meesumqazalbash@gmail.com>
2025-03-23 13:58:59 +0500

    feat(gh-477): LazyLoader for deferred module loading (#500)
    * feat(gh-477): LazyLoader for deferred module loading
    
    * test: add unit tests for `LazyLoader` functionality

--- src/gwkokab/__init__.py
+++ src/gwkokab/__init__.py
@@ -8,16 +8,17 @@ import importlib.metadata
 __version__ = importlib.metadata.version("gwkokab")
 
 
-from . import (
-    constants as constants,
-    cosmology as cosmology,
-    errors as errors,
-    inference as inference,
-    logger as logger,
-    models as models,
-    parameters as parameters,
-    poisson_mean as poisson_mean,
-    population as population,
-    utils as utils,
-    vts as vts,
-)
+from gwkokab.internal import lazy_loader
+
+
+constants = lazy_loader.LazyLoader("constants", globals(), "gwkokab.constants")
+cosmology = lazy_loader.LazyLoader("cosmology", globals(), "gwkokab.cosmology")
+errors = lazy_loader.LazyLoader("errors", globals(), "gwkokab.errors")
+inference = lazy_loader.LazyLoader("inference", globals(), "gwkokab.inference")
+logger = lazy_loader.LazyLoader("logger", globals(), "gwkokab.logger")
+models = lazy_loader.LazyLoader("models", globals(), "gwkokab.models")
+parameters = lazy_loader.LazyLoader("parameters", globals(), "gwkokab.parameters")
+poisson_mean = lazy_loader.LazyLoader("poisson_mean", globals(), "gwkokab.poisson_mean")
+population = lazy_loader.LazyLoader("population", globals(), "gwkokab.population")
+utils = lazy_loader.LazyLoader("utils", globals(), "gwkokab.utils")
+vts = lazy_loader.LazyLoader("vts", globals(), "gwkokab.vts")
