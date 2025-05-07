fix: correction of single event likelihood calculation (#535)
* fix: correction of single event likelihood calculation

* fix: correct sign in log determinant calculation in `PrimaryMassAndMassRatioToComponentMassesTransform`

* fix: improve logsumexp handling in `ScaledMixture` to avoid returning -inf

* fix: ensure proper handling of proposal distributions and log weights in PoissonMean

* fix: correct log determinant sign in `PrimaryMassAndMassRatioToComponentMassesTransform` and `ComponentMassesToPrimaryMassAndMassRatio`

* chore: update `uv.lock`

src/gwkokab/inference/poissonlikelihood.py
@@ -66,6 +66,7 @@ def poisson_likelihood(
 
     # maximum size of the data
     max_size = max([d.shape[0] for d in data])
+    sum_log_size = sum([jnp.log(d.shape[0]) for d in data])
 
     # pad the data and log_ref_priors to the maximum size and create a mask for the data
     # to indicate which elements are valid and which are padded.
@@ -137,18 +138,16 @@ def poisson_likelihood(
         ) -> Tuple[Array, None]:
             data, log_ref_prior, mask = input
 
-            safe_data = jnp.where(mask[:, None], data, jnp.ones_like(data))
-            safe_log_ref_prior = jnp.where(mask, log_ref_prior, jnp.zeros_like(mask))
+            safe_data = jnp.where(mask[:, jnp.newaxis], data, 1.0)
+            safe_log_ref_prior = jnp.where(mask, log_ref_prior, 0.0)
 
             log_prob: Array = model_instance.log_prob(safe_data) - safe_log_ref_prior
-            log_prob = jnp.where(
-                mask, log_prob, jnp.full_like(mask, -jnp.inf, dtype=log_prob.dtype)
-            )
+            log_prob = jnp.where(mask, log_prob, -jnp.inf)
 
             log_prob_sum = jax.nn.logsumexp(
                 log_prob,
                 axis=-1,
-                where=~jnp.isneginf(log_prob),
+                where=(~jnp.isneginf(log_prob)) & mask,
             )
             return carry + log_prob_sum, None
 
@@ -157,15 +156,14 @@ def poisson_likelihood(
             jnp.zeros(()),
             (batched_data, batched_log_ref_priors, batched_mask),
         )
+        total_log_likelihood -= sum_log_size
 
         expected_rates = ERate_fn(model_instance)
         log_prior = priors.log_prob(x)
         log_likelihood = total_log_likelihood - expected_rates
         log_posterior = log_prior + log_likelihood
 
-        log_posterior = jnp.nan_to_num(
-            log_posterior, nan=-jnp.inf, posinf=-jnp.inf, neginf=-jnp.inf
-        )
+        log_posterior = jnp.nan_to_num(log_posterior, nan=-jnp.inf)
 
         return log_posterior
 

src/gwkokab/models/transformations.py
@@ -382,7 +382,7 @@ class ComponentMassesToPrimaryMassAndMassRatio(Transform):
 
     def log_abs_det_jacobian(self, x, y, intermediates=None):
         m1 = x[..., 0]
-        return -jnp.log(m1)
+        return -jnp.log(jnp.abs(m1))
 
     def tree_flatten(self):
         return (), ((), dict())

src/gwkokab/models/utils/_scaledmixture.py
@@ -240,4 +240,8 @@ class ScaledMixture(Distribution):
         safe_sum_log_probs = jnp.where(
             jnp.isneginf(sum_log_probs), -jnp.inf, sum_log_probs
         )
-        return jax.nn.logsumexp(safe_sum_log_probs, axis=-1)
+        return jax.nn.logsumexp(
+            safe_sum_log_probs,
+            where=~jnp.isneginf(sum_log_probs),
+            axis=-1,
+        )