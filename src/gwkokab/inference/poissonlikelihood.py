fix: add log constants to likelihood calculation
log constant contains the factor for the mean of single event likelihood and the number of events detected over the total time.

src/gwkokab/inference/poissonlikelihood.py
@@ -67,6 +67,8 @@ def poisson_likelihood(
     # maximum size of the data
     max_size = max([d.shape[0] for d in data])
     sum_log_size = sum([jnp.log(d.shape[0]) for d in data])
+    log_constants = -sum_log_size
+    log_constants += len(data) * jnp.log(524.1837931943585)
 
     # pad the data and log_ref_priors to the maximum size and create a mask for the data
     # to indicate which elements are valid and which are padded.
@@ -156,11 +158,10 @@ def poisson_likelihood(
             jnp.zeros(()),
             (batched_data, batched_log_ref_priors, batched_mask),
         )
-        total_log_likelihood -= sum_log_size
 
         expected_rates = ERate_fn(model_instance)
         log_prior = priors.log_prob(x)
-        log_likelihood = total_log_likelihood - expected_rates
+        log_likelihood = total_log_likelihood - expected_rates + log_constants
         log_posterior = log_prior + log_likelihood
 
         log_posterior = jnp.nan_to_num(