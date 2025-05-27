3f3084d58f517bb8a9cbc1b66afca01129ca7ad7
Meesum Qazalbash <meesumqazalbash@gmail.com>
2025-05-27 18:52:21 +0500

    Revert "refactor: remove `ecc_matters` module and associated files"
    This reverts commit 7a8ef8645d1394119ddd2f2308fb8ca843c03524.

---
+++ src/kokab/ecc_matters/__init__.py
@@ -0,0 +1,16 @@
+# Copyright 2023 The GWKokab Authors
+# SPDX-License-Identifier: Apache-2.0
+
+
+#
+"""
+.. admonition::
+    🚧 Page under construction 🚧
+
+
+    Documentation for this module is under construction. You can check the source code for more information or
+    run :code:`--help` on the following CLIs:
+
+    - :code:`genie_ecc_matters`
+    - :code:`sage_ecc_matters`
+"""
---
+++ src/kokab/ecc_matters/sage.py
@@ -0,0 +1,124 @@
+# Copyright 2023 The GWKokab Authors
+# SPDX-License-Identifier: Apache-2.0
+
+
+import json
+from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
+from glob import glob
+
+import numpy as np
+from jax import random as jrd
+from loguru import logger
+
+from gwkokab.inference import Bake, poisson_likelihood
+from gwkokab.parameters import ECCENTRICITY, PRIMARY_MASS_SOURCE, SECONDARY_MASS_SOURCE
+from gwkokab.poisson_mean import PoissonMean
+from gwkokab.utils.tools import error_if
+from kokab.ecc_matters.common import EccentricityMattersModel
+from kokab.utils import poisson_mean_parser, sage_parser
+from kokab.utils.common import (
+    flowMC_default_parameters,
+    get_posterior_data,
+    get_processed_priors,
+    read_json,
+    vt_json_read_and_process,
+)
+from kokab.utils.flowMC_helper import flowMChandler
+
+
+def make_parser() -> ArgumentParser:
+    parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
+    parser = sage_parser.get_parser(parser)
+
+    return parser
+
+
+def main() -> None:
+    r"""Main function of the script."""
+    logger.warning(
+        "If you have made any changes to any parameters, please make sure"
+        " that the changes are reflected in scripts that generate plots.",
+    )
+
+    parser = make_parser()
+    args = parser.parse_args()
+
+    SEED = args.seed
+    KEY = jrd.PRNGKey(SEED)
+    KEY1, KEY2, KEY3, KEY4 = jrd.split(KEY, 4)
+    POSTERIOR_REGEX = args.posterior_regex
+    POSTERIOR_COLUMNS = args.posterior_columns
+
+    prior_dict = read_json(args.prior_json)
+
+    model_prior_param = get_processed_priors(
+        ["log_rate", "alpha_m", "mmin", "mmax", "loc", "scale", "low", "high"],
+        prior_dict,
+    )
+
+    model = Bake(EccentricityMattersModel)(**model_prior_param)
+
+    parameters = [PRIMARY_MASS_SOURCE, SECONDARY_MASS_SOURCE, ECCENTRICITY]
+    error_if(
+        set(POSTERIOR_COLUMNS) != set(map(lambda p: p.name, parameters)),
+        msg="The parameters in the posterior data do not match the parameters in the model.",
+    )
+
+    nvt = vt_json_read_and_process([param.name for param in parameters], args.vt_json)
+
+    pmean_kwargs = poisson_mean_parser.poisson_mean_parser(args.pmean_json)
+    erate_estimator = PoissonMean(nvt, key=KEY4, **pmean_kwargs)  # type: ignore[arg-type]
+
+    data = get_posterior_data(glob(POSTERIOR_REGEX), POSTERIOR_COLUMNS)
+    log_ref_priors = [np.zeros(d.shape[:-1]) for d in data]
+
+    variables_index, priors, poisson_likelihood_fn = poisson_likelihood(
+        dist_builder=model,
+        data=data,
+        log_ref_priors=log_ref_priors,
+        ERate_fn=erate_estimator.__call__,
+    )
+
+    constants = model.constants
+
+    with open("constants.json", "w") as f:
+        json.dump(constants, f)
+
+    with open("nf_samples_mapping.json", "w") as f:
+        json.dump(variables_index, f)
+
+    FLOWMC_HANDLER_KWARGS = read_json(args.flowMC_json)
+
+    FLOWMC_HANDLER_KWARGS["sampler_kwargs"]["rng_key"] = KEY1
+    FLOWMC_HANDLER_KWARGS["nf_model_kwargs"]["key"] = KEY2
+
+    N_CHAINS = FLOWMC_HANDLER_KWARGS["sampler_kwargs"]["n_chains"]
+    initial_position = priors.sample(KEY3, (N_CHAINS,))
+
+    FLOWMC_HANDLER_KWARGS["nf_model_kwargs"]["n_features"] = initial_position.shape[1]
+    FLOWMC_HANDLER_KWARGS["sampler_kwargs"]["n_dim"] = initial_position.shape[1]
+
+    FLOWMC_HANDLER_KWARGS["data_dump_kwargs"]["labels"] = list(model.variables.keys())
+
+    FLOWMC_HANDLER_KWARGS = flowMC_default_parameters(**FLOWMC_HANDLER_KWARGS)
+
+    if args.adam_optimizer:
+        from flowMC.strategy.optimization import optimization_Adam
+
+        adam_kwargs = read_json(args.adam_json)
+        Adam_opt = optimization_Adam(**adam_kwargs)
+
+        FLOWMC_HANDLER_KWARGS["sampler_kwargs"]["strategies"] = [Adam_opt, "default"]
+
+    handler = flowMChandler(
+        logpdf=poisson_likelihood_fn,
+        initial_position=initial_position,
+        **FLOWMC_HANDLER_KWARGS,
+    )
+
+    handler.run(
+        debug_nans=args.debug_nans,
+        profile_memory=args.profile_memory,
+        check_leaks=args.check_leaks,
+        file_prefix="ecc_matters",
+    )
