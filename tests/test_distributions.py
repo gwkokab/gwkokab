ee1ba430507e73a654b61af7f5811d67a652a5cd
Meesum Qazalbash <meesumqazalbash@gmail.com>
2025-07-04 20:08:03 +0500

    test: skip tests for `SimpleRedshiftPowerlaw` as it is not a valid probability distribution

--- tests/test_distributions.py
+++ tests/test_distributions.py
@@ -967,6 +967,8 @@ def test_gof(jax_dist, params):
         pytest.skip("Failure rate is lower than expected.")
     if isinstance(jax_dist, ScaledMixture):
         pytest.skip("skip testing for ScaledMixture")
+    if jax_dist.__name__ in ("SimpleRedshiftPowerlaw",):
+        pytest.skip("SimpleRedshiftPowerlaw is not a valid probability distribution")
     num_samples = 10000
     rng_key = jrd.PRNGKey(0)
     d = jax_dist(**params)
