93da59b80f469cef4c2efd53f01930906fd67863
Meesum Qazalbash <meesumqazalbash@gmail.com>
2025-07-12 09:39:11 +0500

    feat: implement `Guru` class for random number generator management

--- src/kokab/utils/__init__.py
+++ src/kokab/utils/__init__.py
@@ -5,6 +5,7 @@
 from . import (
     common as common,
     genie_parser as genie_parser,
+    guru as guru,
     poisson_mean_parser as poisson_mean_parser,
     ppd as ppd,
     ppd_parser as ppd_parser,
