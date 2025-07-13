52e58da38a3c8b8db9018e9ab8c88a7220628074
Meesum Qazalbash <meesumqazalbash@gmail.com>
2025-07-14 01:20:22 +0500

    feat: enhance `analytical_likelihood` function with detailed docstring and improved scan loops to reduce compilation time

--- src/gwkokab/inference/analyticallikelihood.py
+++ src/gwkokab/inference/analyticallikelihood.py
@@ -2,12 +2,13 @@
 # SPDX-License-Identifier: Apache-2.0
 
 
-from typing import Callable, Dict, List, Optional
+from typing import Callable, Dict, List, Optional, Tuple
 
 import jax
 from jax import numpy as jnp, random as jrd
 from jaxtyping import Array, PRNGKeyArray
-from numpyro.distributions import Distribution
+from loguru import logger
+from numpyro.distributions import Distribution, MultivariateNormal
 
 from gwkokab.models.utils import JointDistribution
 
@@ -23,10 +24,61 @@ def analytical_likelihood(
     key: PRNGKeyArray,
     N_samples: int = 10_000,
 ) -> Callable[[Array, Array], Array]:
-    scale_trils = [
-        jnp.linalg.cholesky(covariance_matrix) for covariance_matrix in covariances
-    ]
-    n_dim = means[0].shape[0]
+    r"""Compute the analytical likelihood function for a model given its parameters.
+
+    .. math::
+
+        \mathcal{L}_{\mathrm{analytical}}(\Lambda,\kappa)\propto
+        \exp\left(-\mu(\Lambda,\kappa)\right)
+        \prod_{i=1}^{N}
+        \iint\mathcal{G}(\theta,z|\boldsymbol{\mu}_i,\boldsymbol{\Sigma}_i)
+        \rho(\theta,z\mid\Lambda,\kappa)d\theta dz
+
+        \mathcal{L}_{\mathrm{analytical}}(\Lambda,\kappa)\propto
+        \exp{\left(-\mu(\Lambda,\kappa)\right)}\prod_{i=1}^{N}\bigg<
+        \rho(\theta_{i,j},z_{i,j}\mid\Lambda,\kappa)
+        \bigg>_{\theta_{i,j},z_{i,j}\sim\mathcal{G}(\theta,z|\boldsymbol{\mu}_i,\boldsymbol{\Sigma}_i)}
+
+    Parameters
+    ----------
+    dist_fn : Callable[..., Distribution]
+        function that returns a Distribution instance
+    priors : JointDistribution
+        priors for the model parameters
+    variables_index : Dict[str, int]
+        mapping of variable names to their indices in the input array
+    ERate_fn : Callable[[Distribution, Optional[int]], Array]
+        function to compute the expected event rates
+    redshift_index : Optional[int]
+        index of the redshift variable in the input array, if applicable
+    means : List[Array]
+        List of mean vectors for each event. Each vector should have the same length and
+        should correspond to the parameters of the distribution along with covariances.
+    covariances : List[Array]
+        List of covariance matrices for each event. Each matrix should correspond to the
+        mean vectors in `means`.
+    key : PRNGKeyArray
+        JAX random key for sampling
+    N_samples : int, optional
+        Number of samples to draw from the multivariate normal distribution for each
+        event to compute the likelihood, by default 10_000
+
+    Returns
+    -------
+    Callable[[Array, Array], Array]
+        A function that computes the log posterior probability of the model parameters
+        given the data. The function takes two arguments: an array of model parameters
+        and a second array (not used in this implementation).
+    """
+    n_events = len(means)
+    mean_stack: Array = jax.block_until_ready(
+        jax.device_put(jnp.stack(means, axis=0), may_alias=True)
+    )
+    cov_stack: Array = jax.block_until_ready(
+        jax.device_put(jnp.stack(covariances, axis=0), may_alias=True)
+    )
+    logger.debug("mean_stack.shape: {shape}", shape=mean_stack.shape)
+    logger.debug("cov_stack.shape: {shape}", shape=cov_stack.shape)
 
     def likelihood_fn(x: Array, _: Array) -> Array:
         mapped_params = {
@@ -36,30 +88,32 @@ def analytical_likelihood(
 
         model_instance: Distribution = dist_fn(**mapped_params)
 
-        rng_key = key
-
         # μ = E_{Ω|Λ}[VT(ω)]
         expected_rates = jax.block_until_ready(ERate_fn(model_instance, redshift_index))
 
-        # - Σ log(M_i)
-        total_log_likelihood = -len(means) * jnp.log(N_samples)
-        for mean, scale_tril in zip(means, scale_trils):
-            # See implementation of `numpyro.distributions.MultivariateNormal.sample`
-            # for method to generate samples from a multivariate normal distribution.
-            # ε ~ N(0, I)
-            eps = jrd.normal(rng_key, shape=(N_samples, n_dim))
-            # L_i = cholesky(Σ_i)
-            # data_n = μ_i + L_i * ε
-            data = mean + jnp.squeeze(
-                jnp.matmul(scale_tril, eps[..., jnp.newaxis]), axis=-1
-            )
-            # log ρ(data_n|Λ,κ)
+        def scan_fn(
+            carry: Array, loop_data: Tuple[Array, Array, PRNGKeyArray]
+        ) -> Tuple[Array, None]:
+            mean, cov, rng_key = loop_data
+            mvn = MultivariateNormal(loc=mean, covariance_matrix=cov)
+            # data_i ~ G(θ, z | μ_i, Σ_i)
+            data = mvn.sample(rng_key, (N_samples,))
+            # log ρ(data_i|Λ,κ)
             log_prob = model_instance.log_prob(data)
-            # log Σ exp log p(data_n|Λ,κ)
-            total_log_likelihood += jax.nn.logsumexp(
+            # log Σ exp log p(data_i|Λ,κ)
+            carry += jax.nn.logsumexp(
                 log_prob, axis=-1, where=(~jnp.isneginf(log_prob))
             )
-            (rng_key,) = jrd.split(rng_key, num=1)
+            return carry, None
+
+        keys = jrd.split(key, (n_events,))
+
+        total_log_likelihood, _ = jax.lax.scan(
+            scan_fn,  # type: ignore[arg-type]
+            -n_events * jnp.log(N_samples),  # - Σ log(M_i)
+            (mean_stack, cov_stack, keys),
+            length=n_events,
+        )
 
         # log L(Λ,κ) = -μ + Σ log Σ exp (log ρ(data_n|Λ,κ)) - Σ log(M_i)
         log_likelihood = total_log_likelihood - expected_rates
