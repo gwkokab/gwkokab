27c9842b3487393c43e7bf68d2755423859a6e83
Meesum Qazalbash <meesumqazalbash@gmail.com>
2025-07-15 11:34:23 +0500

    feat: optimize `analytical_likelihood` by introducing JAX's `scan` for iterative fitting and removing unnecessary blocking
    * Removed `jax.block_until_ready` clause
    * Wrap fitting mean logic inside a scan loop
    * Utilized `jax.vmap` inside fitting mean scan loop for model log prob
    * Wrap scan functions inside `jax.jit`

--- src/gwkokab/inference/analyticallikelihood.py
+++ src/gwkokab/inference/analyticallikelihood.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 
+import functools as ft
 from typing import Callable, Dict, List, Optional, Tuple
 
 import jax
@@ -13,6 +14,7 @@ from numpyro.distributions import Distribution, MultivariateNormal
 from gwkokab.models.utils import JointDistribution
 
 
+@ft.partial(jax.jit, static_argnames=("n_samples",))
 def _mvn_samples(
     loc: Array, scale_tril: Array, n_samples: int, key: PRNGKeyArray
 ) -> Array:
@@ -119,7 +121,7 @@ def analytical_likelihood(
         model_instance: Distribution = dist_fn(**mapped_params)
 
         # μ = E_{Ω|Λ}[VT(ω)]
-        expected_rates = jax.block_until_ready(ERate_fn(model_instance, redshift_index))
+        expected_rates = ERate_fn(model_instance, redshift_index)
 
         event_mvn = MultivariateNormal(loc=mean_stack, covariance_matrix=cov_stack)
 
@@ -128,26 +130,39 @@ def analytical_likelihood(
         # Σ_f = Σ_i
         fit_scale_tril = event_mvn.scale_tril  # not changing the scale_tril here
 
-        for _ in range(max_iter):
+        @jax.jit
+        def scan_fit_fn(fit_mean: Array, key: PRNGKeyArray) -> Tuple[Array, None]:
             fit_samples = _mvn_samples(
                 loc=fit_mean,
                 scale_tril=fit_scale_tril,
                 n_samples=1_000,
-                key=rng_key,
+                key=key,
             )
 
             # weights = ρ(fit_samples | Λ, κ) * G(θ, z | μ_i, Σ_i))
             weights = jnp.expand_dims(
                 jnp.exp(
-                    model_instance.log_prob(fit_samples)
+                    jax.vmap(model_instance.log_prob, in_axes=(1,), out_axes=-1)(
+                        fit_samples
+                    )
                     + event_mvn.log_prob(fit_samples)
                 ),
                 axis=-1,
             )
             # μ_f = sum(weights * fit_samples) / sum(weights)
-            fit_mean = jnp.sum(fit_samples * weights, axis=0) / jnp.sum(weights, axis=0)
-            (rng_key,) = jrd.split(rng_key, num=1)
+            new_fit_mean = jnp.sum(fit_samples * weights, axis=0) / jnp.sum(
+                weights, axis=0
+            )
+            return new_fit_mean, None
 
+        fit_mean, _ = jax.lax.scan(
+            scan_fit_fn,  # type: ignore[arg-type]
+            fit_mean,
+            jrd.split(rng_key, max_iter),
+            length=max_iter,
+        )
+
+        @jax.jit
         def scan_fn(
             carry: Array, loop_data: Tuple[Array, Array, Array, PRNGKeyArray]
         ) -> Tuple[Array, None]:
@@ -180,8 +195,6 @@ def analytical_likelihood(
             (mean_stack, cov_stack, fit_mean, keys),
             length=n_events,
         )
-
-        total_log_likelihood = jax.block_until_ready(total_log_likelihood)
 
         # log L(Λ,κ) = -μ + Σ log Σ exp (log ρ(data_n|Λ,κ)) - Σ log(M_i)
         log_likelihood = total_log_likelihood - expected_rates
