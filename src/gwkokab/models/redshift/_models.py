fix: adjust log probability calculations in VolumetricPowerlawRedshift and SimpleRedshiftPowerlaw classes by subtracting normalization term

src/gwkokab/models/redshift/_models.py
@@ -147,7 +147,7 @@ class VolumetricPowerlawRedshift(Distribution):
         ArrayLike
             Log-probability values.
         """
-        return self.log_differential_spacetime_volume(value)
+        return self.log_differential_spacetime_volume(value) - self.log_norm()
 
 
 class SimpleRedshiftPowerlaw(Distribution):
@@ -242,4 +242,4 @@ class SimpleRedshiftPowerlaw(Distribution):
         ArrayLike
             Log-probability values.
         """
-        return self.kappa * jnp.log1p(value)
+        return self.kappa * jnp.log1p(value) - self.log_norm()