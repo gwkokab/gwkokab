7fb21cb429fc6929ceee72294e674bbe8ce3df08
Meesum Qazalbash <meesumqazalbash@gmail.com>
2025-08-02 00:25:18 +0500

    refactor: update constants to use Final type and replace C with SPEED_OF_LIGHT

--- src/gwkokab/constants.py
+++ src/gwkokab/constants.py
@@ -4,13 +4,12 @@
 #
 """All necessary constants for the package."""
 
-C = 299792458.0
+from typing import Final
+
+
+SPEED_OF_LIGHT: Final[float] = 299792458.0
 r"""Speed of light in vacuum in :math:`\text{m}/\text{s}`"""
-MSUN_SI = 1.9884099021470415e30
+MSUN_SI: Final[float] = 1.9884099021470415e30
 """Solar mass in kg."""
-DEFAULT_DZ = 1e-3
-"""Default redshift step size for cosmology calculations."""
-SECONDS_PER_YEAR = 365.25 * 24 * 60 * 60
+SECONDS_PER_YEAR: Final[float] = 365.25 * 24 * 60 * 60
 """Seconds in 1 year."""
-Mpc3_to_Gpc3 = 1e-9
-"""Conversion from Mpc^3 to Gpc^3."""
