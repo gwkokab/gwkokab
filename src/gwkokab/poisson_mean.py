fix: replace VolumetricPowerlawRedshift with SimpleRedshiftPowerlaw in model and update log probability calculation in PoissonMean class

src/gwkokab/poisson_mean.py
@@ -289,6 +289,17 @@ class PoissonMean(eqx.Module):
             # log p(θ_i|λ) - log w_i
             log_prob = model_log_prob - log_weights
 
+            z = jax.lax.dynamic_index_in_dim(
+                samples, redshift_index, axis=-1, keepdims=False
+            )
+
+            log_prob += (
+                LN_4_PI
+                + LN_MPC3_TO_GPC3
+                + PLANCK_2015_Cosmology.logdVcdz(z)
+                - jnp.log1p(z)
+            )
+
             partial_logsumexp = jnn.logsumexp(
                 log_prob, where=~jnp.isneginf(log_prob), axis=-1
             )
@@ -324,8 +335,10 @@ class PoissonMean(eqx.Module):
                 axis=0,
             )
 
+        scale_factor = self.time_scale / num_samples
+
         # (T / n_total) * exp(log Σ exp(log p(θ_i|λ) - log w_i))
-        return (self.time_scale / num_samples) * jnp.exp(
+        return scale_factor * jnp.exp(
             jnn.logsumexp(log_prob, where=~jnp.isneginf(log_prob), axis=-1)
         )
 