fix: disable `prevent_cse` in `jax.checkpoint` for model log probability calculation

src/gwkokab/inference/poissonlikelihood.py
@@ -144,7 +144,7 @@ def poisson_likelihood(
             # log p(ω|data_n)
             model_log_prob = jax.checkpoint(
                 jax.vmap(model_instance.log_prob),
-                prevent_cse=True,
+                prevent_cse=False,
             )(safe_data)
             safe_model_log_prob = jnp.where(mask, model_log_prob, -jnp.inf)
 