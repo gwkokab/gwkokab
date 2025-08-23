91e2bcd836a6f02a770dba264e05c8d1414cb01d
Meesum Qazalbash <meesumqazalbash@gmail.com>
2025-08-24 03:45:26 +0500

    refactor(init): replace `nsage_parser` import with `nsage`

--- src/kokab/utils/__init__.py
+++ src/kokab/utils/__init__.py
@@ -7,7 +7,7 @@ from . import (
     genie_parser as genie_parser,
     guru as guru,
     monk as monk,
-    nsage_parser as nsage_parser,
+    nsage as nsage,
     poisson_mean_parser as poisson_mean_parser,
     ppd as ppd,
     ppd_parser as ppd_parser,
