a43d0bbe1571cb9d4333d174f0708076ad117af6
Meesum Qazalbash <meesumqazalbash@gmail.com>
2025-09-01 15:54:18 +0500

    fix: add secondary mass source parameter to main function

--- src/kokab/n_pls_m_gs/genie.py
+++ src/kokab/n_pls_m_gs/genie.py
@@ -273,7 +273,10 @@ def main() -> None:
         ("mmin_pl", N_pl),
     ]
 
-    parameters_name: Tuple[str, ...] = (Parameters.PRIMARY_MASS_SOURCE.value,)
+    parameters_name: Tuple[str, ...] = (
+        Parameters.PRIMARY_MASS_SOURCE.value,
+        Parameters.SECONDARY_MASS_SOURCE.value,
+    )
 
     error_magazine.register(
         lambda x, size, key: banana_error_m1_m2(
