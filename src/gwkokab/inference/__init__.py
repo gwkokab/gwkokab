b3cd894d2b8151d13639016f231a48fcf1de0b68
Meesum Qazalbash <meesumqazalbash@gmail.com>
2025-09-01 12:14:16 +0500

    refactor: reorganize analytical likelihood module structure

--- src/gwkokab/inference/__init__.py
+++ src/gwkokab/inference/__init__.py
@@ -4,7 +4,9 @@
 #
 """Provides essential classes and functions for the inference module."""
 
-from .analyticallikelihood import analytical_likelihood as analytical_likelihood
+from ._analytical_likelihood.analyticallikelihood import (
+    analytical_likelihood as analytical_likelihood,
+)
 from .poissonlikelihood import poisson_likelihood as poisson_likelihood
 from .poissonlikelihood_numpyro import (
     numpyro_poisson_likelihood as numpyro_poisson_likelihood,
---
+++ src/gwkokab/inference/_analytical_likelihood/__init__.py
@@ -0,0 +1,5 @@
+# Copyright 2023 The GWKokab Authors
+# SPDX-License-Identifier: Apache-2.0
+
+
+from .analyticallikelihood import analytical_likelihood as analytical_likelihood
