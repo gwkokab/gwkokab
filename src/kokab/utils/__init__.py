4d9caa1aab66ad5aa0082bdcef85f3f6a5a93b5b
Meesum Qazalbash <meesumqazalbash@gmail.com>
2025-09-02 09:48:41 +0500

    fix: remove `f_monk` import

--- src/kokab/utils/__init__.py
+++ src/kokab/utils/__init__.py
@@ -4,10 +4,10 @@
 
 from . import (
     common as common,
-    f_monk as f_monk,
     flowMC_based as flowMC_based,
     genie_parser as genie_parser,
     guru as guru,
+    monk as monk,
     poisson_mean_parser as poisson_mean_parser,
     ppd as ppd,
     ppd_parser as ppd_parser,
