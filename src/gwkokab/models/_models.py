fix(gh-619): simplify Beta distribution parameters calculation and enhance spin model checks

src/gwkokab/models/_models.py
@@ -330,7 +330,16 @@ class SmoothedTwoComponentPrimaryMassRatio(Distribution):
         m1, q = jnp.unstack(value, axis=-1)
         m2 = m1 * q
         log_smoothing_q = log_planck_taper_window((m2 - self.mmin) / self.delta)
-        log_prob_q = self.beta * jnp.log(q) + log_smoothing_q
+        log_prob_q = (
+            doubly_truncated_power_law_log_prob(
+                x=q,
+                alpha=self.beta,
+                low=jnp.divide(self.mmin, m1),
+                high=1.0,
+            )
+            + log_smoothing_q
+        )
+
         return log_prob_q
 
     @validate_sample

src/gwkokab/models/spin/_models.py
@@ -171,15 +171,6 @@ def BetaFromMeanVar(
     Beta
         Beta distribution with the specified mean and variance.
     """
-    safe_variance = jnp.where(variance <= 0, 1.0, variance)
-    alpha = jnp.where(
-        variance <= 0,
-        1.0,
-        (jnp.square(mean) * (1 - mean) - mean * variance) / safe_variance,
-    )
-    beta = jnp.where(
-        variance <= 0,
-        1.0,
-        (mean * jnp.square(1 - mean) - (1 - mean) * variance) / safe_variance,
-    )
+    alpha = (jnp.square(mean) * (1 - mean) - mean * variance) / variance
+    beta = (mean * jnp.square(1 - mean) - (1 - mean) * variance) / variance
     return Beta(alpha, beta, validate_args=validate_args)