d13372a5db48af1a033c24ca4ebcc64f53903b11
Meesum Qazalbash <meesumqazalbash@gmail.com>
2025-10-10 01:14:26 +0500

    feat: add `LazyJointDistribution` for Lazy Priors and related functionality

--- tests/kokab/utils/test_common.py
+++ tests/kokab/utils/test_common.py
@@ -2,12 +2,10 @@
 # SPDX-License-Identifier: Apache-2.0
 
 
-from typing import Dict
-
 import pytest
 from numpyro import distributions as dist
 
-from kokab.utils.common import get_dist, get_posterior_data
+from kokab.utils.common import _available_prior, get_posterior_data
 
 
 def test_get_posterior_data():
@@ -16,22 +14,18 @@ def test_get_posterior_data():
 
 
 @pytest.mark.parametrize(
-    "meta_dict",
+    "dist_name",
     [
-        {"dist": "Uniform", "low": -5, "high": 10},
-        {"dist": "Normal", "loc": 0.1, "scale": 10},
-        {"dist": "HalfNormal", "scale": 10},
-        {"dist": "Uniform", "low": 0, "high": 0},
-        {"dist": "Normal", "loc": 0, "scale": 0},
+        "Uniform",
+        "Normal",
+        "HalfNormal",
+        "Uniform",
+        "Normal",
         pytest.param(
-            {"dist": "InvalidDist", "param": 1}, marks=pytest.mark.xfail(strict=True)
+            "InvalidDist", marks=pytest.mark.xfail(strict=True)
         ),  # Invalid distribution
     ],
 )
-def test_get_dist(meta_dict: Dict) -> None:
-    parsed_dist = get_dist(meta_dict.copy())
-    assert isinstance(parsed_dist, dist.Distribution)
-    dist_name: str = meta_dict.pop("dist")
-    assert parsed_dist.__class__.__name__.lower() == dist_name.lower()
-    for key, value in meta_dict.items():
-        assert getattr(parsed_dist, key) == value
+def test_available_prior(dist_name: str) -> None:
+    parsed_dist = _available_prior(dist_name)
+    assert issubclass(parsed_dist, dist.Distribution)
