5dc25bc48a710125d54ec87c2c374fc47b5aeea9
Meesum Qazalbash <meesumqazalbash@gmail.com>
2025-10-10 22:26:35 +0500

    feat(gh-135): add `BrokenPowerlawTwoPeak` distribution implementation (#643)
    * feat(gh-135): add `BrokenPowerlawTwoPeak` distribution implementation
    
    * feat: add `BrokenPowerlawTwoPeakFull` model implementation
    
    * feat: import `BrokenPowerlawTwoPeak` from mass models
    
    * feat: add `BrokenPowerlawTwoPeak` and `BrokenPowerlawTwoPeakFull` implementations with associated CLI scripts
    
    * fix: `BrokenPowerlawTwoPeak` to include two minimum masses
    
    * feat: enhance `BrokenPowerlawTwoPeak` with additional argument constraints and validation checks
    
    * fix: update parameter names in `BrokenPowerlawTwoPeakFull`
    
    * feat: add BrokenPowerlawTwoPeakFullMonk class with argument parsing and model options
    
    * feat: add f_monk_bp2p script to Monk scripts section in pyproject.toml
    
    * feat: add eccentricity parameter support in `BrokenPowerlawTwoPeakFull` and its `Monk` integration

--- src/gwkokab/models/__init__.py
+++ src/gwkokab/models/__init__.py
@@ -13,10 +13,12 @@ from . import (
     utils as utils,
 )
 from .hybrids import (
+    BrokenPowerlawTwoPeakFull as BrokenPowerlawTwoPeakFull,
     NPowerlawMGaussian as NPowerlawMGaussian,
     PowerlawPeak as PowerlawPeak,
 )
 from .mass import (
+    BrokenPowerlawTwoPeak as BrokenPowerlawTwoPeak,
     PowerlawPrimaryMassRatio as PowerlawPrimaryMassRatio,
     SmoothedTwoComponentPrimaryMassRatio as SmoothedTwoComponentPrimaryMassRatio,
     Wysocki2019MassModel as Wysocki2019MassModel,
--- src/gwkokab/models/hybrids/__init__.py
+++ src/gwkokab/models/hybrids/__init__.py
@@ -2,5 +2,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
 
+from ._bp2pfull import BrokenPowerlawTwoPeakFull as BrokenPowerlawTwoPeakFull
 from ._npowerlawmgaussian import NPowerlawMGaussian as NPowerlawMGaussian
 from ._powerlawpeak import PowerlawPeak as PowerlawPeak
