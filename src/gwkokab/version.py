62ac023d044952edff92dd43ec11f29af9c9638c
Meesum Qazalbash <meesumqazalbash@gmail.com>
2025-11-24 01:43:30 +0500

    refactor(version): streamline version string construction and improve commit hash retrieval

--- src/gwkokab/version.py
+++ src/gwkokab/version.py
@@ -2,6 +2,9 @@
 # SPDX-License-Identifier: Apache-2.0
 
 
+__all__ = ["__version__"]
+
+
 import datetime
 import os
 import subprocess
@@ -11,11 +14,11 @@ from typing import Final, Optional
 MAJOR_VERSION: Final[int] = 0
 MINOR_VERSION: Final[int] = 2
 PATCH_VERSION: Final[int] = 1
-BASE_VERSION: Final[str] = f"{MAJOR_VERSION}.{MINOR_VERSION}.{PATCH_VERSION}"
 
 
 def get_git_commit_hash() -> Optional[str]:
-    """Attempts to get the commit hash from the git repository.
+    """Attempts to get the commit hash from the git repository. Uses
+    :code:`git rev-parse HEAD` to retrieve the current commit hash if possible.
 
     Returns
     -------
@@ -23,7 +26,6 @@ def get_git_commit_hash() -> Optional[str]:
         The git commit hash or None if unavailable.
     """
     try:
-        # Use 'git rev-parse HEAD' to get the commit hash
         result = subprocess.run(
             ["git", "rev-parse", "HEAD"],
             capture_output=True,
@@ -50,20 +52,20 @@ def get_version() -> str:
         nightly builds.
     """
     NIGHTLY_ENV_VAR = "GWKOKAB_NIGHTLY_BUILD"
+    version = f"{MAJOR_VERSION}.{MINOR_VERSION}.{PATCH_VERSION}"
 
     if os.environ.get(NIGHTLY_ENV_VAR) is None:
         # Return the clean base version for standard release builds.
-        return BASE_VERSION
+        return version
 
     commit_hash = get_git_commit_hash()
 
     if commit_hash:
-        version = f"{BASE_VERSION}+g{commit_hash}"
+        version += f"+g{commit_hash}"
     else:
-        karachi_tz = datetime.timezone(datetime.timedelta(hours=5))
         # not considering time because it changes too frequently
-        now = datetime.datetime.now(tz=karachi_tz).strftime("%Y%m%d")
-        version = f"{BASE_VERSION}+d{now}"
+        utc_now = datetime.datetime.now().strftime("%Y%m%d")
+        version += f"+d{utc_now}"
 
     return version
 
