fix: simplify prior calculation in load_injection_data and apply_injection_prior functions

src/gwkokab/poisson_mean/_injection_based_helper.py
@@ -272,9 +272,7 @@ def load_injection_data(
         if (
             "sampling_pdf" in data
         ):  # O1+O2+O3 mixture and endO3 injections (https://dcc.ligo.org/LIGO-T2100377, https://dcc.ligo.org/LIGO-T2100113)
-            gwpop_data["prior"] = np.asarray(
-                data["sampling_pdf"][()][found]
-            ) * np.square(2 * np.pi * gwpop_data["a_1"] * gwpop_data["a_2"])
+            gwpop_data["prior"] = np.asarray(data["sampling_pdf"][()][found])
         elif (
             "lnpdraw_mass1_source_mass2_source_redshift_spin1x_spin1y_spin1z_spin2x_spin2y_spin2z"
             in data
@@ -285,7 +283,6 @@ def load_injection_data(
                         "lnpdraw_mass1_source_mass2_source_redshift_spin1x_spin1y_spin1z_spin2x_spin2y_spin2z"
                     ][()][found]
                 )
-                + 2.0 * np.log(2 * np.pi * gwpop_data["a_1"] * gwpop_data["a_2"])
             )
         else:  # O4a sensitivity injections (https://dcc.ligo.org/LIGO-T2400073)
             gwpop_data["prior"] = np.exp(
@@ -321,6 +318,11 @@ def apply_injection_prior(data: Dict[str, Array], parameters: List[str]):
     """We assume the injection prior in terms of the source frame primary mass and mass
     ratio.
     """
+
+    if P.PRIMARY_SPIN_MAGNITUDE in parameters:
+        data["prior"] *= 2 * np.pi * data[P.PRIMARY_SPIN_MAGNITUDE]
+    if P.SECONDARY_SPIN_MAGNITUDE in parameters:
+        data["prior"] *= 2 * np.pi * data[P.SECONDARY_SPIN_MAGNITUDE]
 
     if P.MASS_RATIO in parameters:
         data[P.MASS_RATIO] = data[P.SECONDARY_MASS_SOURCE] / data[P.PRIMARY_MASS_SOURCE]