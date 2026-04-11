5b0dad3486c7c934670b12976f05a453d5d80a46
Meesum Qazalbash <meesumqazalbash@gmail.com>
2026-04-11 22:48:03 +0500

    feat: coherent error induction in injections with flexibility to choose select coordinates

--- src/gwkokab/errors.py
+++ src/gwkokab/errors.py
@@ -4,27 +4,26 @@
 
 from typing import Optional
 
+import numpy as np
 import RIFT.lalsimutils as lalsimutils
 from jax import numpy as jnp, random as jrd
-from jaxtyping import Array, PRNGKeyArray
-
-from .utils.transformations import chirp_mass, symmetric_mass_ratio
+from jaxtyping import PRNGKeyArray
 
 
 __all__ = [
-    "banana_error_m1_m2",
+    "banana_error",
     "truncated_normal_error",
 ]
 
 
-def banana_error_m1_m2(
-    x: Array,
+def banana_error(
+    x: np.ndarray,
     size: int,
     key: PRNGKeyArray,
     *,
     scale_Mc: float = 1.0,
     scale_eta: float = 1.0,
-) -> Array:
+) -> np.ndarray:
     r"""Add banana error to the given values. Section 3 of the `Model-independent
     inference on compact-binary observations <https://doi.org/10.1093/mnras/stw2883>`_
     discusses the banana error. It adds errors in the chirp mass and symmetric mass
@@ -38,7 +37,7 @@ def banana_error_m1_m2(
         \eta = \eta^{T}
         \left[1+0.03\frac{12}{\rho}\left(r_{0}^{'}+r^{'}\right)\right]
 
-    x : Array
+    x : np.ndarray
         given values as m1 and m2
     size : int
         number of samples
@@ -51,58 +50,50 @@ def banana_error_m1_m2(
 
     Returns
     -------
-    Array
-        m1 and m2 with banana
+    np.ndarray
+        array of values with added banana error
     """
-    m1 = x[..., 0]
-    m2 = x[..., 1]
-
     keys = jrd.split(key, 5)
 
-    r0 = jrd.normal(key=keys[0])
-    r0p = jrd.normal(key=keys[1])
-    r = jrd.normal(key=keys[2], shape=(size,)) * scale_Mc
-    rp = jrd.normal(key=keys[3], shape=(size,)) * scale_eta
-    rho = 9.0 * jnp.power(jrd.uniform(key=keys[4]), -1.0 / 3.0)
+    r0 = np.asarray(jrd.normal(key=keys[0]))
+    r0p = np.asarray(jrd.normal(key=keys[1]))
+    r = np.asarray(jrd.normal(key=keys[2], shape=(size,))) * scale_Mc
+    rp = np.asarray(jrd.normal(key=keys[3], shape=(size,))) * scale_eta
+    rho = 9.0 * np.power(np.asarray(jrd.uniform(key=keys[4])), -1.0 / 3.0)
 
-    Mc_true = chirp_mass(m1=m1, m2=m2)
-    eta_true = symmetric_mass_ratio(m1=m1, m2=m2)
+    Mc_true, eta_true = np.unstack(x, axis=-1)
 
-    v_PN_param = (jnp.pi * Mc_true * 20 * lalsimutils.MsunInSec) ** (
+    v_PN_param = (np.pi * Mc_true * 20 * lalsimutils.MsunInSec) ** (
         1.0 / 3.0
     )  # 'v' parameter
     v_PN_param_max = 0.2
-    v_PN_param = jnp.min(jnp.array([v_PN_param, v_PN_param_max]))
+    v_PN_param = np.min(np.array([v_PN_param, v_PN_param_max]))
     snr_fac = rho / 12.0
     # this ignores range due to redshift / distance, based on a low-order est
     ln_mc_error_pseudo_fisher = (
         1.5 * 0.3 * (v_PN_param / v_PN_param_max) ** (7.0) / snr_fac
     )
 
-    beta = jnp.min(jnp.array([0.07 / snr_fac, ln_mc_error_pseudo_fisher]))
+    beta = np.min(np.array([0.07 / snr_fac, ln_mc_error_pseudo_fisher]))
 
     Mc = Mc_true * (1.0 + beta * (r0 + r))
     eta = eta_true * (1.0 + 0.03 * (12.0 / rho) * (r0p + rp))
 
-    etaV = 1.0 - 4.0 * eta
-    etaV_sqrt = jnp.where(etaV >= 0, jnp.sqrt(etaV), jnp.nan)
+    Mc = np.where(Mc <= 0.0, np.nan, Mc)
+    eta = np.where((eta <= 0.25) & (eta >= 0.0), eta, np.nan)
 
-    factor = 0.5 * Mc * jnp.power(eta, -0.6)
-    m1_final = factor * (1.0 + etaV_sqrt)
-    m2_final = factor * (1.0 - etaV_sqrt)
-
-    return jnp.column_stack([m1_final, m2_final])
+    return np.stack((Mc, eta), axis=-1)
 
 
 def truncated_normal_error(
-    x: Array,
+    x: np.ndarray,
     size: int,
     key: PRNGKeyArray,
     *,
     scale: float,
     low: Optional[float] = None,
     high: Optional[float] = None,
-) -> Array:
+) -> np.ndarray:
     """Adds truncated normal error to the given values. The error is sampled from a
     truncated normal distribution with the given parameters. The function will resample
     until all values are within the allowed range.
@@ -119,7 +110,7 @@ def truncated_normal_error(
 
     Parameters
     ----------
-    x : Array
+    x : np.ndarray
         Given values to which the error will be added.
     size : int
         Number of samples to generate.
@@ -134,15 +125,18 @@ def truncated_normal_error(
 
     Returns
     -------
-    Array
-        Array of values with added truncated normal error.
+    np.ndarray
+        Array of values with added truncated normal error, with all values within the
+        specified bounds.
     """
     key_r0, key_r, key_rho = jrd.split(key, 3)
 
-    r0 = jrd.normal(key=key_r0)
-    r = jrd.normal(key=key_r, shape=(size,))
-    rho = 9.0 * jnp.power(
-        jrd.uniform(key=key_rho, minval=jnp.finfo(jnp.result_type(float)).tiny),
+    r0 = np.asarray(jrd.normal(key=key_r0))
+    r = np.asarray(jrd.normal(key=key_r, shape=(size,)))
+    rho = 9.0 * np.power(
+        np.asarray(
+            jrd.uniform(key=key_rho, minval=jnp.finfo(jnp.result_type(float)).tiny)
+        ),
         -1.0 / 3.0,
     )
 
@@ -150,11 +144,11 @@ def truncated_normal_error(
 
     # reflect samples that are out of bounds back into the allowed range
     if low is not None and high is not None:
-        samples = low + jnp.mod(samples - low, 2 * (high - low))
-        samples = jnp.where(samples > high, 2.0 * high - samples, samples)
+        samples = low + np.mod(samples - low, 2 * (high - low))
+        samples = np.where(samples > high, 2.0 * high - samples, samples)
     elif low is not None:
-        samples = jnp.where(samples < low, 2.0 * low - samples, samples)
+        samples = np.where(samples < low, 2.0 * low - samples, samples)
     elif high is not None:
-        samples = jnp.where(samples > high, 2.0 * high - samples, samples)
+        samples = np.where(samples > high, 2.0 * high - samples, samples)
 
     return samples
