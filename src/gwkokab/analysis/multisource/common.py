e447c5fae44c2a6eb683340117014cce65e1e4a6
Meesum Qazalbash <meesumqazalbash@gmail.com>
2026-05-30 22:23:07 +0500

    fix: reorder parameters in `MultiSourceModelCore`

--- src/gwkokab/analysis/multisource/common.py
+++ src/gwkokab/analysis/multisource/common.py
@@ -171,9 +171,9 @@ class MultiSourceModelCore:
                     "m1_alpha2_",
                     "m1_break_",
                     "m1_delta_",
-                    "m2_delta_",
                     "m1_high_",
                     "m1_low_",
+                    "m2_delta_",
                     "m2_low_",
                 ])
             if ct == "gpl":
